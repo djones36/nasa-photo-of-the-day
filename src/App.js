d99fd538a952571d59ba5f6404a729c20552ebc0
Daniel Jones <daniel.jones2000@gmail.com>
2019-08-14 14:12:07 -0700

    created button div

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import ReloadButton from "./components/button"
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
+      <ReloadButton/>
     </div>
   );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+Const ReloadButton = () => {
+    return(
+        <button className="reload-button">
+            Reload
+        </button>
+    );
+}
+
+export default ReloadButton;
